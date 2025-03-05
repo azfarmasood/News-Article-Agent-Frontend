b86db4bbef182ad59dab7b3be5cc09fce82d229a
Syed Hafiz Azfar Masood <138787481+azfarmasood@users.noreply.github.com>
2025-03-06 04:11:12 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import NewsPage from "@/components/widgets/practice/check";
 import DailyMaleNews from "@/components/widgets/practice/DailyMale";
 import Footer from "@/components/widgets/practice/Footer";
-import GaurdianNews from "@/components/widgets/practice/Gurdian";
+// import GaurdianNews from "@/components/widgets/practice/Gurdian";
 import IndependentNews from "@/components/widgets/practice/Independent";
 import Navbar from "@/components/widgets/practice/Navbar";
 import TelegraphNews from "@/components/widgets/practice/Telegraph";
